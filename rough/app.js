35deb3d58003a85a72bc47bacdc33b7f8708df00
Anurag <kinganurag8@gmail.com>
2020-09-09 19:35:53 +0530

    added youtube search function

--- rough/app.js
+++ rough/app.js
@@ -1,20 +1,27 @@
 const express = require('express')
 const { static } = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.use(static("public"))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/",function(req,res){
     res.render("home.ejs"); 
 })
 
-app.get("/view/:name",function(req,res){
-    var name=req.params.name; 
+var name="";
+app.get("/view",function(req,res){
     res.render("star.ejs",{
-        name:name
+        name:name,
     })
 });
+
+app.post("/addStar",function(req,res){
+    name=req.body.name;
+    res.redirect("/view");
+})
 
 app.get("*",function(req,res){
     res.send("<h1>Page Not Found</h1>")
