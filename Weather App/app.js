9931c8507061294df17adcc7813096e22324e910
Anurag <kinganurag8@gmail.com>
2020-09-16 03:30:51 +0530

    added precise location feature and metric units

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,31 +1,36 @@
 const express = require('express')
-const { static } = require('express')
+const { static, response } = require('express')
 const bodyParser = require('body-parser')
 const request=require('request');
 
 const app = express()
 const port = 3000
 const base="http://api.openweathermap.org/data/2.5/weather?APPID=4ee4236d370e0116fe0e87f937f90445&zip="
-
+const pin="https://api.postalpincode.in/pincode/"
 
 app.use(static("public"))
 app.use(bodyParser.urlencoded({ extended: false }))
 var zip="";
 var country="";
 var url="";
+var pinUrl="";
+var temp=0.0;
+var city="";
+var weather="";
+var data;
 
 app.get('/', function(req, res){
     res.render("home.ejs");
 })
 
 app.get('/weather',function(req,res){
-    request(url,(error,response)=>{
-        const data=JSON.parse(response.body);
-        console.log(data);
-        const temp=(parseFloat(data["main"]["temp"])-273.15).toFixed(2);;
+    request(pinUrl,(error,response)=>{
+        data=JSON.parse(response.body);
+        city=data[0]["PostOffice"][0]["District"]+", "+data[0]["PostOffice"][0]["State"]
         res.render("weather.ejs",{
-            weather:data["weather"][0]["main"],
-            temp:temp
+            weather:weather,
+            temp:temp,
+            city:city
         })
     })
 })
@@ -34,7 +39,13 @@ app.post('/location',function(req,res){
     zip=req.body.zip;
     country=req.body.country;
     url=base+zip+","+country;
-    res.redirect('/weather')
+    pinUrl=pin+zip;
+    request(url,(error,response)=>{
+        data=JSON.parse(response.body);
+        temp=(parseFloat(data["main"]["temp"])-273.15).toFixed(2);
+        weather=data["weather"][0]["main"];
+        res.redirect('/weather');
+    });
 })
 
 app.listen(port, () => console.log(`Server is running!`))
