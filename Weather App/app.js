72e7b595eb27d6fc74cd944de49ad26ae878a924
Anurag <kinganurag8@gmail.com>
2020-09-16 02:35:29 +0530

    integrated OWM backend API with app

--- Weather App/app.js
+++ Weather App/app.js
@@ -5,26 +5,35 @@ const request=require('request');
 
 const app = express()
 const port = 3000
+const base="http://api.openweathermap.org/data/2.5/weather?APPID=4ee4236d370e0116fe0e87f937f90445&zip="
+
 
 app.use(static("public"))
 app.use(bodyParser.urlencoded({ extended: false }))
-var zip=0;
+var zip="";
 var country="";
+var url="";
 
 app.get('/', function(req, res){
     res.render("home.ejs");
 })
 
 app.get('/weather',function(req,res){
-    res.render("weather.ejs",{
-        zip:zip,
-        country:country
+    request(url,(error,response)=>{
+        const data=JSON.parse(response.body);
+        console.log(data);
+        const temp=(parseFloat(data["main"]["temp"])-273.15).toFixed(2);;
+        res.render("weather.ejs",{
+            weather:data["weather"][0]["main"],
+            temp:temp
+        })
     })
 })
 
 app.post('/location',function(req,res){
     zip=req.body.zip;
     country=req.body.country;
+    url=base+zip+","+country;
     res.redirect('/weather')
 })
 
